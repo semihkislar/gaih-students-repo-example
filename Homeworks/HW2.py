2f87cfcd39c520404ae1675bc0d8bab5fa9f57f3
Semih Kışlar <semih.kislar@gmail.com>
2021-03-11 19:32:41 +0300

    HW2 completed

---
+++ Homeworks/HW2.py
@@ -0,0 +1,16 @@
+allCvs = [
+    {'name': 'Semih', 'surName': 'Kışlar',
+        'title': 'Software Developer', 'company': 'web.tv'},
+    {'name': 'Salih', 'surName': 'Gencer',
+        'title': 'Team Lead', 'company': 'turkticaret.net'},
+    {'name': 'Mert', 'surName': 'Bıçakcı',
+        'title': 'Key Account Manager', 'company': 'theadx.com'},
+    {'name': 'Aydın', 'surName': 'Akan',
+        'title': 'Full Stack Developer', 'company': 'BEINSports'},
+    {'name': 'Seyhan', 'surName': 'Koçak',
+        'title': 'Talent Hunter', 'company': 'turkticaret.net'},
+]
+
+for i in allCvs:
+    print(i['name'], 'works in', i['company'], 'as a', i['title'])
+
